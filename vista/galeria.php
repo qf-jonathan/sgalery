f54b57936486b942a7bd95630b7a5fceed7251bd
Di3trich <qf.jonathan@gmail.com>
2012-06-06 13:59:07 -0500

    cambios

--- vista/galeria.php
+++ vista/galeria.php
@@ -13,8 +13,18 @@
 			
 		</div>
 		<div id="pie">
-			<div id="navegador"></div>
-			<div id="subir"></div>
+			<div id="navegador">
+				<div id="imagenes">
+					<?php for($i=0; $i<100;$i++):?>
+					<div style="display: table-cell; padding:10px; border: solid 1px red; height: 124px; width: 124px;">---------------------</div>
+					<?php endfor; ?>
+				</div>
+			</div>
+			<div id="subir">
+				<form action="index.php?c=galeria&a=guardar_imagen" method="post">
+					<input type="file"/>
+				</form>
+			</div>
 		</div>
     </body>
 </html>
