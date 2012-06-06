8907c8fb977801812cbb9df8be15134e39ac340d
Di3trich <qf.jonathan@gmail.com>
2012-06-06 04:10:21 -0500

    añadido jquery, controlador de galeria y vista galeria

---
+++ controlador/galeria.php
@@ -0,0 +1,17 @@
+<?php
+
+class Galeria_Controlador extends Controlador {
+
+	public function __construct() {
+		$this->cargar_vista('galeria');
+	}
+	
+	public function index_accion(){
+		
+	}
+	
+	public function guardar_imagen_accion(){
+		
+	}
+
+}
